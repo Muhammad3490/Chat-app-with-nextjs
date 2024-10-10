eae59651f8c380420ee476dc8bab9689aead5624
Muhammad3490 <muhg56190@gmail.com>
2024-10-10 05:35:24 -0700

    Update db.ts

--- lib/db.ts
+++ lib/db.ts
@@ -1,21 +1,12 @@
 import { PrismaClient } from "@prisma/client";
 
-declare global {
-    var prisma: PrismaClient | undefined;
-}
-
-// Wrap PrismaClient initialization in a try-catch block for error handling
 let prismaClient: PrismaClient;
 
-try {
-    prismaClient = new PrismaClient();
-} catch (error) {
-    console.error("Error initializing Prisma Client:", error);
-    throw new Error("Failed to initialize Prisma Client");
-}
+const getPrismaClient = () => {
+    if (!prismaClient) {
+        prismaClient = new PrismaClient();
+    }
+    return prismaClient;
+};
 
-export const db = globalThis.prisma || prismaClient;
-
-if (process.env.NODE_ENV !== "production") {
-    globalThis.prisma = db; // Ensure we are using globalThis
-}
+export const db = getPrismaClient();
